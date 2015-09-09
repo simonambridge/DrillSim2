c8e361c9ce8754ee8851bdd24241935b072811c5
Simon Ambridge <simon@ntier-db.com>
2015-09-09 22:48:40 +0100

    Bit form complete bar save routine. 9994 Pascal lines

--- FormBitData.pas
+++ FormBitData.pas
@@ -6,7 +6,9 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  CheckLst;
+  DrillSimVariables,
+  DrillSimUtilities,
+  DrillSimMessageToMemo;
 
 type
 
@@ -21,14 +23,27 @@ type
     Jet1Label: TLabel;
     Jet3ComboBox: TComboBox;
     Jet2Label: TLabel;
-    Jet3ComboBox1: TComboBox;
+    Jet4ComboBox: TComboBox;
     Jet3Label: TLabel;
     Jet4Label: TLabel;
     BitTypeLabel: TLabel;
-    MaxJetsLabel: TLabel;
     MaxJetsData: TLabel;
+    MaxJetsLabel: TLabel;
     QuitButton: TButton;
     SaveButton: TButton;
+    procedure BitNumberDataChange(Sender: TObject);
+    procedure BitTypeDataChange(Sender: TObject);
+    procedure NumericOnlyKeyPress(Sender: TObject; var Key: Char);
+    Procedure JetCount;
+    procedure FormActivate(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDeActivate(Sender: TObject);
+    procedure Jet1ComboBoxChange(Sender: TObject);
+    procedure Jet2ComboBoxChange(Sender: TObject);
+    procedure Jet3ComboBoxChange(Sender: TObject);
+    procedure Jet4ComboBoxChange(Sender: TObject);
+    procedure QuitButtonClick(Sender: TObject);
+    procedure SaveButtonClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -37,10 +52,242 @@ type
 
 var
   BitDataForm: TBitDataForm;
+  _WellMaxJets                     : integer;
+  _WellJet                         : array[1..4] of integer;
+  _WellBitNumber                   : integer;
+  _WellBitType                     : String20;
+
 
 implementation
 
 {$R *.lfm}
+
+{ TBitDataForm }
+
+{ ------------- Edit Procedures ------------ }
+
+procedure TBitDataForm.NumericOnlyKeyPress(Sender: TObject; var Key: Char);
+begin
+  if not (Key in [#8, '0'..'9', '-', DefaultFormatSettings.DecimalSeparator]) then
+  Begin
+    Key := #0;  // discard it
+  end
+  else if ((Key = DefaultFormatSettings.DecimalSeparator) or (Key = '-')) and
+          (Pos(Key, (Sender as TEdit).Text) > 0) then
+  Begin
+    Key := #0;  // discard it
+  end
+  else if (Key = '.') and
+          ((Sender as TEdit).SelStart = 0) then
+  Begin
+    Key := #0;  // discard it
+  end;
+end;
+
+Procedure TBitDataForm.JetCount;
+var i : integer;
+Begin
+  _WellMaxJets:=0;
+  for i:=1 to 4
+  do
+  Begin
+    if _WellJet[i]>0 then _WellMaxJets:=_WellMaxJets+1;
+  End;
+  MaxJetsData.Caption:=IntToStr(_WellMaxJets);
+End;
+
+//
+
+procedure TBitDataForm.BitNumberDataChange(Sender: TObject);
+begin
+  if not ((TEdit(Sender).Text = '-')
+      or (TEdit(Sender).Text = DefaultFormatSettings.DecimalSeparator)
+      or (TEdit(Sender).Text = ''))
+          then _WellBitNumber:=StrToInt(BitNumberData.Text);
+end;
+
+procedure TBitDataForm.BitTypeDataChange(Sender: TObject);
+begin
+  if TEdit(Sender).Text <> ''
+          then _WellBitType:=BitTypeData.Text;
+end;
+
+procedure TBitDataForm.Jet1ComboBoxChange(Sender: TObject);
+begin
+  if not ((TEdit(Sender).Text = '-')
+      or (TEdit(Sender).Text = DefaultFormatSettings.DecimalSeparator)
+      or (TEdit(Sender).Text = ''))
+          then _WellJet[1]:=Jet1ComboBox.ItemIndex; { jet 1 diameter n/32" }
+  if _WellJet[1]>0 then
+  Begin
+    Jet1ComboBox.Color:=clDefault;
+    Jet1ComboBox.Font.Color:=clBlack;
+  end else
+  Begin
+    Jet1ComboBox.Color:=clRed;
+    Jet1ComboBox.Font.Color:=clWhite;
+  End;
+  JetCount;
+end;
+
+procedure TBitDataForm.Jet2ComboBoxChange(Sender: TObject);
+begin
+  if not ((TEdit(Sender).Text = '-')
+      or (TEdit(Sender).Text = DefaultFormatSettings.DecimalSeparator)
+      or (TEdit(Sender).Text = ''))
+          then _WellJet[2]:=Jet2ComboBox.ItemIndex; { jet 2 diameter n/32" }
+  if _WellJet[2]>0 then
+  Begin
+    Jet2ComboBox.Color:=clDefault;
+    Jet2ComboBox.Font.Color:=clBlack;
+  end else
+  Begin
+    Jet2ComboBox.Color:=clRed;
+    Jet2ComboBox.Font.Color:=clWhite;
+  End;
+  JetCount;
+end;
+
+procedure TBitDataForm.Jet3ComboBoxChange(Sender: TObject);
+begin
+  if not ((TEdit(Sender).Text = '-')
+      or (TEdit(Sender).Text = DefaultFormatSettings.DecimalSeparator)
+      or (TEdit(Sender).Text = ''))
+          then _WellJet[3]:=Jet3ComboBox.ItemIndex; { jet 3 diameter n/32" }
+  if _WellJet[3]>0 then
+  Begin
+    Jet3ComboBox.Color:=clDefault;
+    Jet3ComboBox.Font.Color:=clBlack;
+  end else
+  Begin
+    Jet3ComboBox.Color:=clRed;
+    Jet3ComboBox.Font.Color:=clWhite;
+  End;
+  JetCount;
+end;
+
+procedure TBitDataForm.Jet4ComboBoxChange(Sender: TObject);
+begin
+  if not ((TEdit(Sender).Text = '-')
+      or (TEdit(Sender).Text = DefaultFormatSettings.DecimalSeparator)
+      or (TEdit(Sender).Text = ''))
+          then _WellJet[4]:=Jet4ComboBox.ItemIndex; { jet 4 diameter n/32" }
+  if _WellJet[4]>0 then
+  Begin
+    Jet4ComboBox.Color:=clDefault;
+    Jet4ComboBox.Font.Color:=clBlack;
+  end else
+  Begin
+    Jet4ComboBox.Color:=clRed;
+    Jet4ComboBox.Font.Color:=clWhite;
+  End;
+  JetCount;
+end;
+
+{ ------------------ Form procedures ---------------------- }
+
+
+procedure TBitDataForm.FormActivate(Sender: TObject);
+var i : integer;
+begin
+  StringToMemo('Form Bit Data activated...........................');
+
+  // units of measure labels
+  { no UoM for this form }
+
+  // data
+
+  _WellBitNumber:=Data.BitNumber;
+  _WellBitType:=Data.BitType;
+  _WellMaxJets:=0;
+
+  for i:=1 to 4 do
+  Begin
+    Case i of
+      1: Begin
+           if Data.Jet[1]>0 then
+           Begin
+             Jet1ComboBox.Color:=clDefault;
+             Jet1ComboBox.Font.Color:=clBlack;
+             _WellJet[i]:=Data.Jet[i];
+             _WellMaxJets:=_WellMaxJets+1;
+             Jet1ComboBox.ItemIndex:=Data.Jet[1];
+           end else
+           Begin
+             Jet1ComboBox.Color:=clRed;
+             Jet1ComboBox.Font.Color:=clWhite;
+           End;
+         End;
+      2: Begin
+           if Data.Jet[2]>0 then
+           Begin
+             Jet2ComboBox.Color:=clDefault;
+             Jet2ComboBox.Font.Color:=clBlack;
+             _WellJet[i]:=Data.Jet[i];
+             _WellMaxJets:=_WellMaxJets+1;
+             Jet2ComboBox.ItemIndex:=Data.Jet[2];
+           end else
+           Begin
+             Jet2ComboBox.Color:=clRed;
+             Jet2ComboBox.Font.Color:=clWhite;
+           End;
+         End;
+      3: Begin
+           if Data.Jet[3]>0 then
+           Begin
+             Jet3ComboBox.Color:=clDefault;
+             Jet3ComboBox.Font.Color:=clBlack;
+             _WellJet[i]:=Data.Jet[i];
+             _WellMaxJets:=_WellMaxJets+1;
+             Jet3ComboBox.ItemIndex:=Data.Jet[3];
+           end else
+           Begin
+             Jet3ComboBox.Color:=clRed;
+             Jet3ComboBox.Font.Color:=clWhite;
+           End;
+         End;
+      4: Begin
+           if Data.Jet[4]>0 then
+           Begin
+             Jet4ComboBox.Color:=clDefault;
+             Jet4ComboBox.Font.Color:=clBlack;
+             _WellJet[i]:=Data.Jet[i];
+             _WellMaxJets:=_WellMaxJets+1;
+             Jet4ComboBox.ItemIndex:=Data.Jet[4];
+           end else
+           Begin
+             Jet4ComboBox.Color:=clRed;
+             Jet4ComboBox.Font.Color:=clWhite;
+           End;
+         End;
+    end;
+  end;
+  JetCount;
+end;
+
+procedure TBitDataForm.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TBitDataForm.FormDeActivate(Sender: TObject);
+begin{ Also called on QuitButton }
+    StringToMemo('FormBitData.Deactivate ........................................');
+end;
+
+
+procedure TBitDataForm.QuitButtonClick(Sender: TObject);
+begin
+  StringToMemo('FormBitData.Quit ..............................................');
+  Close;
+end;
+
+procedure TBitDataForm.SaveButtonClick(Sender: TObject);
+begin
+{ --- stuff here ---- }
+  Edited:=True;
+  Close;
+end;
 
 end.
 
