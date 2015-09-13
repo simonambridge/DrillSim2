54ec6926252ee6116717eb26acc1c7096e71c496
Simon Ambridge <simon@ntier-db.com>
2015-09-13 21:59:21 +0100

    Working on Pump form, update to generic NumericOnlyKeyPress. 10657 Pascal lines

--- FormHoleData.pas
+++ FormHoleData.pas
@@ -98,22 +98,30 @@ implementation
 
       {$R *.lfm}
 { ------------- Edit Procedures ------------ }
-procedure THoleDataForm.NumericOnlyKeyPress(Sender: TObject; var Key: char);
+
+procedure THoleDataForm.NumericOnlyKeyPress(Sender: TObject; var Key: Char);
 begin
-  if not (Key in [#8, '0'..'9', '-', DefaultFormatSettings.DecimalSeparator]) then
-  begin
-    Key := #0; // discard it
+  if not (Key in [#8, '0'..'9', '-', DefaultFormatSettings.DecimalSeparator]) then   { discard if not in approved list of chars }
+  Begin
+    Key := #0;  // discard it
   end
-  else if ((Key = DefaultFormatSettings.DecimalSeparator) or (Key = '-')) and
-    (Pos(Key, (Sender as TEdit).Text) > 0) then
-  begin
-    Key := #0; // discard it
+  else if ((Key = '-')) and  ((Sender as TEdit).SelStart <> 0) then             { discard if "-" and not first char in string }
+  Begin
+    Key := #0;  // discard it
   end
-  else if (Key = '.') and ((Sender as TEdit).SelStart = 0) then
-  begin
-    Key := #0; // discard it
+  else if ((Key = DefaultFormatSettings.DecimalSeparator) or (Key = '-')) and        { discard if "." or "-" already in string }
+                (Pos(Key, (Sender as TEdit).Text) > 0) then
+  Begin
+    Key := #0;  // discard it
+  end
+  else if (Key = DefaultFormatSettings.DecimalSeparator) and                         { discard if "." is first charachter }
+                ((Sender as TEdit).SelStart = 0) then
+  Begin
+    Key := #0;  // discard it
   end;
 end;
+
+
 
 procedure THoleDataForm.CasingYNRadioGroupClick(Sender: TObject);
 begin
